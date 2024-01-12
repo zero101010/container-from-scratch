7023f5de396c8c5d2ba24ecfbfe81e08233415a8
Igor araujo de Sousa <iasousa.platform@rd.com.br>
2024-01-12 17:32:22 -0300

    Copy the arch base files to insid of the container and update the proc path

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func run() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 
 	if err := cmd.Run(); err != nil {
@@ -39,8 +39,24 @@ func child() {
 	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	// Set hostname of the new UTS namespace
-	if err := syscall.Sethostname([]byte("HMcontainer")); err != nil {
+	if err := syscall.Sethostname([]byte("IgorContainer")); err != nil {
 		fmt.Println("Error setting hostname:", err)
+		os.Exit(1)
+	}
+	// Change to the new root file system.
+	if err := syscall.Chroot("/home/ubuntu/rootfs"); err != nil {
+		fmt.Println("Error changing root:", err)
+		os.Exit(1)
+	}
+	// Change working directory after changing the root.
+	if err := os.Chdir("/"); err != nil {
+		fmt.Println("Error changing working directory:", err)
+		os.Exit(1)
+	}
+	// Mount proc. This needs to be done after chroot and chdir.
+	// Because I need the /proc inside of our container that was Copy by chroot and chdir
+	if err := syscall.Mount("proc", "proc", "proc", 0, ""); err != nil {
+		fmt.Println("Error mounting proc:", err)
 		os.Exit(1)
 	}
 
